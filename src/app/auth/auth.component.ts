b4ef85ba754f8184e6b31ce2e626cc1d62df6f50
savanpatel <savanpatel39@gmail.com>
2020-07-06 01:31:59 -0400

    Completed: Authemtication | Incomplete: Match Password and Password Validation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
+  loginSignupForm: FormGroup;
+  pwd = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginSignupForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      cnfrmPassword: new FormControl(null, [Validators.required, Validators.minLength(6), this.matchPassword.bind(this)] )
+    });
+  }
+
+  matchPassword(control: FormControl): {[s: string]: boolean} {
+    if ( this.pwd && control.value !== this.pwd) {
+      console.log('password does not match');
+      return { nomatch : true };
+    }
+    return { match : true };
+  }
+
+  onSubmit() {
+    //
+    const email = this.loginSignupForm.value.email;
+    const password = this.loginSignupForm.value.password;
+    console.log({email, password});
+    this.loginSignupForm.reset();
   }
 
 }
